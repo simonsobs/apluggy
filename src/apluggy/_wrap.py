6f324926fbc6d0a08630676d62e321dd7aee766a
Tai Sakuma <tai.sakuma@gmail.com>
2023-02-20 20:16:19 -0500

    clean code

--- src/apluggy/_wrap.py
+++ src/apluggy/_wrap.py
@@ -122,11 +122,6 @@ class _AHook:
 
 
 class _With:
-    @dataclass
-    class _Context:
-        context: contextlib._GeneratorContextManager
-        stop_iteration: Optional[StopIteration] = None
-
     def __init__(self, pm: PluginManager_):
         self.pm = pm
 
@@ -135,70 +130,80 @@ class _With:
         def call(*args, **kwargs) -> Generator[List, Any, List]:
             hook = getattr(self.pm.hook, name)
             with contextlib.ExitStack() as stack:
-                imps = hook(*args, **kwargs)
-                yields = [stack.enter_context(imp) for imp in imps]
+                hook_impls = hook(*args, **kwargs)
+                yields = [stack.enter_context(imp) for imp in hook_impls]
 
                 # yield yields
 
-                # With the above line uncommented, this function could end here
+                # This function could end here with the above line uncommented
                 # for a normal usage of context managers.
 
-                # The following code
-                # 1. supports `send()` through the `gen` attribute.'
-                #    (https://stackoverflow.com/a/68304565/7309855)
-                # 2. returns the return values of the hook implementations.
-                # 3. and supports `throw()` through the `gen` attribute.
+                # Instead, yield from another generator method that supports
+                # `send()` and `throw()` and returns the return values of the
+                # hook implementations.
 
-                # TODO: Support `close()`.
-
-                contexts = [self._Context(imp) for imp in imps]
-
-                while True:
-                    try:
-                        sent = yield yields
-                    except BaseException as thrown:
-                        # gen.throw() has been called.
-                        # Throw the exception to all hook implementations
-                        # that have not exited.
-                        raised: List[BaseException] = []
-                        for c in contexts:
-                            if c.stop_iteration:
-                                continue
-                            try:
-                                c.context.gen.throw(thrown)
-                            except StopIteration:
-                                pass
-                            except BaseException as e:
-                                raised.append(e)
-                        if raised:
-                            raise BaseExceptionGroup(
-                                'Raised in hook implementations.', raised
-                            )
-                        raise
-
-                    yields = []
-                    for c in contexts:
-                        y = None
-                        if not c.stop_iteration:
-                            try:
-                                y = c.context.gen.send(sent)
-                            except StopIteration as e:
-                                c.stop_iteration = e
-                        yields.append(y)
-
-                    if all(c.stop_iteration for c in contexts):
-                        # All hook implementations have exited.
-                        # Collect return values from StopIteration.
-                        returns = [
-                            c.stop_iteration and c.stop_iteration.value
-                            for c in contexts
-                        ]
-                        return returns
-
-            assert False, 'unreachable'
-            return []  # for mypy
+                returns = yield from self._support_gen(yields, hook_impls)
+            return returns
 
         return call
+
+    def _support_gen(
+        self, yields: List, hook_impls: List[contextlib._GeneratorContextManager]
+    ) -> Generator[List, Any, List]:
+        '''This generator method
+        1. supports `send()` through the `gen` attribute
+           (https://stackoverflow.com/a/68304565/7309855),
+        2. supports `throw()` through the `gen` attribute,
+        3. and returns the return values of the hook implementations.
+
+        TODO: Support `close()`.
+        '''
+
+        @dataclass
+        class _Context:
+            context: contextlib._GeneratorContextManager
+            stop_iteration: Optional[StopIteration] = None
+
+        contexts = [_Context(context=imp) for imp in hook_impls]
+
+        while True:
+            try:
+                sent = yield yields
+            except BaseException as thrown:
+                # gen.throw() has been called.
+                # Throw the exception to all hook implementations
+                # that have not exited.
+                raised: List[BaseException] = []
+                for c in contexts:
+                    if c.stop_iteration:
+                        continue
+                    try:
+                        c.context.gen.throw(thrown)
+                    except StopIteration:
+                        pass
+                    except BaseException as e:
+                        raised.append(e)
+                if raised:
+                    raise BaseExceptionGroup('Raised in hook implementations.', raised)
+                raise
+
+            yields = []
+            for c in contexts:
+                y = None
+                if not c.stop_iteration:
+                    try:
+                        y = c.context.gen.send(sent)
+                    except StopIteration as e:
+                        c.stop_iteration = e
+                yields.append(y)
+
+            if all(c.stop_iteration for c in contexts):
+                # All hook implementations have exited.
+                # Collect return values from StopIteration.
+                returns = [
+                    c.stop_iteration and c.stop_iteration.value for c in contexts
+                ]
+                return returns
 
 
 class _AWith:
