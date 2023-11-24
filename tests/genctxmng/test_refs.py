991d52c1119e65edfaf738a3f95111be0c00fc0f
Tai Sakuma <tai.sakuma@gmail.com>
2023-11-24 14:45:28 -0500

    Clean code

--- tests/genctxmng/test_refs.py
+++ tests/genctxmng/test_refs.py
@@ -12,7 +12,6 @@ def test_refs(data: st.DataObject):
     n_contexts = data.draw(st.integers(min_value=0, max_value=3), label='n_contexts')
 
     n_sends = data.draw(st.integers(min_value=0, max_value=5), label='n_sends')
-    # n_sends = 1
     draw = RecordReturns(data.draw)
 
     ref_imp = nested_with
@@ -21,8 +20,6 @@ def test_refs(data: st.DataObject):
     probe0, yields0 = run(
         draw=draw, stack=ref_imp, n_contexts=n_contexts, n_sends=n_sends
     )
-
-    # ic(probe0.calls)
 
     # Verify the replay draw by running on the same implementation.
     replay = ReplayReturns(draw)
@@ -34,13 +31,12 @@ def test_refs(data: st.DataObject):
     assert yields0 == yields1
 
     # Compare with manual enter/exit implementation.
-    if n_contexts <= 3:
-        replay = ReplayReturns(draw)
-        probe1, yields1 = run(
-            draw=replay, stack=dunder_enter, n_contexts=n_contexts, n_sends=n_sends
-        )
-        assert probe0.calls == probe1.calls
-        assert yields0 == yields1
+    replay = ReplayReturns(draw)
+    probe1, yields1 = run(
+        draw=replay, stack=dunder_enter, n_contexts=n_contexts, n_sends=n_sends
+    )
+    assert probe0.calls == probe1.calls
+    assert yields0 == yields1
 
     if n_sends == 0:
         # Compare with ExitStack, which doesn't support send/throw/close.
