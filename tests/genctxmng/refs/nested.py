1f58664cf8c263c065b6a6c3b5ea4e2c11e3e5c5
Tai Sakuma <tai.sakuma@gmail.com>
2023-11-24 11:43:34 -0500

    Add nested_with_double()

--- tests/genctxmng/refs/nested.py
+++ tests/genctxmng/refs/nested.py
@@ -11,8 +11,8 @@ def nested_with(ctxs: Sequence[GenCtxMngr[T]]) -> GenCtxMngr[list[T]]:
     match len(ctxs):
         case 1:
             return nested_with_single(ctxs)
-        # case 2:
-        #     return stack_with_double(ctxs)
+        case 2:
+            return nested_with_double(ctxs)
         # case 3:
         #     return stack_with_triple(ctxs)
         case _:
@@ -34,3 +34,28 @@ def nested_with_single(ctxs: Sequence[GenCtxMngr[T]]) -> Generator[list[T], Any,
             except StopIteration:
                 break
             sent = yield ys
+
+
+@contextlib.contextmanager
+def nested_with_double(ctxs: Sequence[GenCtxMngr[T]]) -> Generator[list[T], Any, Any]:
+    assert len(ctxs) == 2
+    ctx0, ctx1 = ctxs
+    active = set(ctxs)
+    with ctx0 as y0, ctx1 as y1:
+        ys = [y0, y1]
+        while active:
+            sent = yield ys
+            ys = []
+            try:
+                try:
+                    if ctx1 in active:
+                        y1 = ctx1.gen.send(sent)
+                        ys.append(y1)
+                except StopIteration:
+                    active.remove(ctx1)
+
+                if ctx0 in active:
+                    y0 = ctx0.gen.send(sent)
+                    ys.append(y0)
+            except StopIteration:
+                active.remove(ctx0)
