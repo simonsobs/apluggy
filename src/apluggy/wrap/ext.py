c242d34da6807eddcfb4ff8a4460e8f3b4d9f274
Tai Sakuma <tai.sakuma@gmail.com>
2023-12-06 11:23:46 -0500

    Use async_stack_gen_ctxs() in AWith

--- src/apluggy/wrap/ext.py
+++ src/apluggy/wrap/ext.py
@@ -1,12 +1,11 @@
 import asyncio
-import contextlib
-from collections.abc import AsyncIterator, Callable
+from collections.abc import Callable
 from typing import Any, Coroutine
 
 from pluggy import HookCaller
 from pluggy import PluginManager as PluginManager_
 
-from apluggy.stack import AGenCtxMngr, GenCtxMngr, stack_gen_ctxs
+from apluggy.stack import AGenCtxMngr, GenCtxMngr, async_stack_gen_ctxs, stack_gen_ctxs
 
 
 class AHook:
@@ -46,43 +45,14 @@ class AWith:
 
     def __getattr__(self, name: str) -> Callable[..., AGenCtxMngr]:
         hook: HookCaller = getattr(self.pm.hook, name)
-        return _Call(hook, reverse=self.reverse)
 
+        def call(*args: Any, **kwargs: Any) -> AGenCtxMngr[list]:
+            ctxs = hook(*args, **kwargs)
+            if self.reverse:
+                ctxs = list(reversed(ctxs))
 
-def _Call(
-    hook: Callable[..., list[AGenCtxMngr]], reverse: bool = False
-) -> Callable[..., AGenCtxMngr]:
-    @contextlib.asynccontextmanager
-    async def call(*args: Any, **kwargs: Any) -> AsyncIterator[list]:
-        ctxs = hook(*args, **kwargs)
-        if reverse:
-            ctxs = list(reversed(ctxs))
-        async with contextlib.AsyncExitStack() as stack:
-            yields = [await stack.enter_async_context(ctx) for ctx in ctxs]
+            # TODO: Make `sequential` configurable.  It is set to `True` for
+            # now because nextline-graphql doesn't work with `False`.
+            return async_stack_gen_ctxs(ctxs, sequential=True)
 
-            # TODO: Consider entering the contexts asynchronously as in the
-            # following commented out code.
-
-            # yields = await asyncio.gather(
-            #     *[stack.enter_async_context(ctx) for ctx in ctxs]
-            # )
-
-            yield yields
-
-            # TODO: The following commented out code is an attempt to support
-            # `asend()` through the `gen` attribute. It only works for
-            # simple cases. It doesn't work with starlette.lifespan().
-            # When starlette is shutting down, an exception is raised
-            # `RuntimeError: generator didn't stop after athrow()`.
-
-            # stop = False
-            # while not stop:
-            #     sent = yield yields
-            #     try:
-            #         yields = await asyncio.gather(
-            #             *[ctx.gen.asend(sent) for ctx in ctxs]
-            #         )
-            #     except StopAsyncIteration:
-            #         stop = True
-
-    return call
+        return call
