f6535559ca6c4423ce174bbbde04432f3d826203
Tai Sakuma <tai.sakuma@gmail.com>
2025-01-27 17:59:29 -0500

    Add `pragma` and replace `pass` with `...` for coverage

--- src/apluggy/_decorator.py
+++ src/apluggy/_decorator.py
@@ -53,7 +53,7 @@ class AsyncContextManager(contextlib._AsyncGeneratorContextManager):
     def __init__(self, g, *a, **k):
         super().__init__(g, a, k)
 
-    def __call__(self, func):
+    def __call__(self, func):  # pragma: no cover
         async def caller(f, *a, **k):
             async with self.__class__(self.func, *self.args, **self.kwds):
                 return f(*a, **k)
