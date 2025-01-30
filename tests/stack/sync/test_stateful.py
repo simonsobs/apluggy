513051a63b0fe05b5bb5677ea615a1fdfc6fbc76
Tai Sakuma <tai.sakuma@gmail.com>
2025-01-30 15:27:38 -0500

    Update `test_stateful.py`

--- tests/stack/sync/test_stateful.py
+++ tests/stack/sync/test_stateful.py
@@ -1,17 +1,68 @@
-from collections.abc import Callable, Generator, Iterator
+from collections.abc import Callable, Generator, Iterable, Iterator
 from contextlib import contextmanager
 from hmac import new
 from itertools import count
 from types import TracebackType
-from typing import Optional, Union
+from typing import Optional, TypeVar, Union
 
 from hypothesis import given, settings
 from hypothesis import strategies as st
 
 from apluggy import stack_gen_ctxs
 from apluggy.stack import GenCtxMngr
+from tests.utils import st_none_or
 
 from .refs import dunder_enter, exit_stack, nested_with
+
+T = TypeVar('T')
+
+
+def take_until(condition: Callable[[T], bool], iterable: Iterable[T]) -> Iterator[T]:
+    '''Iterate until after yielding the first element that satisfies the condition.'''
+    for x in iterable:
+        yield x
+        if condition(x):
+            break
+
+
+@st.composite
+def st_iter_until(
+    draw: st.DrawFn,
+    st_: st.SearchStrategy[T],
+    /,
+    *,
+    last: T,
+    max_size: Optional[int] = None,
+) -> Iterator[T]:
+    '''A strategy for iterators that draw from `st_` until `last` is drawn.'''
+    counts = range(max_size) if max_size is not None else count()
+    gen = (draw(st_) for _ in counts)
+    return take_until(lambda x: x == last, gen)
+
+
+@given(...)
+def test_take_until(items: list[int], last: int) -> None:
+    actual = list(take_until(lambda x: x == last, iter(items)))
+    expected = items[: items.index(last) + 1] if last in items else items
+    assert actual == expected
+
+
+@given(data=st.data())
+def test_iterable_until(data: st.DataObject) -> None:
+    samples = data.draw(st.lists(st.text(), min_size=1))
+    st_ = st.sampled_from(samples)
+    last = data.draw(st_)
+    max_size = data.draw(st_none_or(st.integers(min_value=0, max_value=10)))
+
+    it = data.draw(st_iter_until(st_, last=last, max_size=max_size))
+    res = list(it)
+
+    assert last not in res[:-1]
+
+    if max_size is None:
+        assert last == res[-1]
+    else:
+        assert len(res) == max_size or (last == res[-1] and len(res) < max_size)
 
 
 class MockException(Exception):
@@ -86,27 +137,46 @@ class MockContext:
 
     def before_raise(self, exc: Exception) -> None:
         self._raised_expected.clear()
-        self._except_action.clear()
-        ACTIONS = ('handle', 'reraise', 'raise')
-        for id in reversed(self._entered):
-            action = self._draw(st.sampled_from(ACTIONS))
+        self._except_action = self._draw_except_actions(reversed(self._entered))
+
+        for id, (action, exc1) in self._except_action.items():
             if action == 'handle':
                 self._raised_expected.append((id, exc))
-                self._except_action[id] = (action, None)
                 self._handled_expected = True
                 self._raised_on_exit_expected = None
                 break
             elif action == 'reraise':
                 self._raised_expected.append((id, exc))
-                self._except_action[id] = (action, None)
             elif action == 'raise':
                 self._raised_expected.append((id, exc))
-                exc = MockException(f'{id}')
-                self._except_action[id] = (action, exc)
+                assert exc1 is not None
+                exc = exc1
                 self._handled_expected = None
                 self._raised_on_exit_expected = exc
-        ic(self._raised_expected)
-        ic(self._except_action)
+
+    def _draw_except_actions(
+        self, ids: Iterable[int]
+    ) -> dict[int, tuple[str, Union[None, Exception]]]:
+        # e.g., [4, 3, 2, 1]
+        ids = list(ids)
+
+        ACTIONS = ('handle', 'reraise', 'raise')
+        last = 'handle'
+        st_actions = st.sampled_from(ACTIONS)
+
+        # e.g., ['reraise', 'reraise', 'raise', 'handle']
+        actions = self._draw(st_iter_until(st_actions, last=last, max_size=len(ids)))
+
+        # e.g., {
+        #     4: ('reraise', None),
+        #     3: ('reraise', None),
+        #     2: ('raise', MockException('2')),
+        #     1: ('handle', None),
+        # }
+        return {
+            id: (a, MockException(f'{id}') if a == 'raise' else None)
+            for id, a in zip(ids, actions)
+        }
 
 
 class StatefulTest:
@@ -169,23 +239,23 @@ class StatefulTest:
         exc_value: Optional[BaseException],
         traceback: Optional[TracebackType],
     ) -> Optional[bool]:
-        assert exc_value is self._raised
-        ic(exc_value)
+        # assert exc_value is self._raised
+        # ic(exc_value)
         handled: Union[bool, None] = None
         raised: Union[Exception, None] = None
         try:
             handled = self._obj.__exit__(exc_type, exc_value, traceback)
         except Exception as e:
             raised = e
-        ic(handled)
+        # ic(handled)
         self._mock_context.assert_exited(handled=handled, raised=raised)
         return True
 
 
-@settings(max_examples=100)
+@settings(max_examples=500)
 @given(data=st.data())
 def test_property(data: st.DataObject) -> None:
-    print()
+    # print()
     test = StatefulTest(data)
 
     methods = data.draw(st.lists(st.sampled_from(test.methods)))
