2b7c90236b0fe462f9de40f1729c8c8e6a7c1296
Tai Sakuma <tai.sakuma@gmail.com>
2025-02-03 12:03:03 -0500

    Add docstrings

--- tests/stack/sync/exc.py
+++ tests/stack/sync/exc.py
@@ -23,6 +23,67 @@ class MockException(Exception):
 
 @dataclass
 class ExceptionExpectation:
+    '''A comparison of exceptions in a specified way.
+
+    Examples
+    --------
+
+    1. Compare if the exception is the same object.
+
+    >>> exc1 = ValueError('1')
+    >>> exp = ExceptionExpectation(exc1, method='is')
+
+    >>> exp == exc1
+    True
+
+    >>> exp == ValueError('1')
+    False
+
+    Comparisons can be made in the other order as well.
+
+    >>> exc1 == exp
+    True
+
+    >>> ValueError('1') == exp
+    False
+
+
+    2. Compare if the class and message are as expected.
+
+    >>> exp = ExceptionExpectation(exc1, method='type-msg')
+
+    >>> exp == exc1
+    True
+
+    >>> exp == ValueError('1')
+    True
+
+    >>> exp == ValueError('2')
+    False
+
+    A class can be a subclass of the expected class.
+
+    >>> class MyValueError(ValueError):
+    ...     pass
+
+    >>> exp == MyValueError('1')
+    True
+
+
+    3. Compare if the class is as expected.
+
+    >>> exp = ExceptionExpectation(exc1, method='type')
+
+    >>> exp == ValueError('2')
+    True
+
+    >>> exp == MyValueError('2')
+    True
+
+    >>> exp == TypeError('2')
+    False
+    '''
+
     Method = Literal['is', 'type-msg', 'type']
     METHODS: ClassVar[tuple[Method, ...]] = ('is', 'type-msg', 'type')
 
@@ -40,8 +101,12 @@ class ExceptionExpectation:
             return isinstance(other, type(self.expected))
         raise ValueError(self.method)  # pragma: no cover
 
+
 def _generator_did_not_yield() -> RuntimeError:
-    '''Return the exception raised on entering a context manager.'''
+    '''Return the exception raised on entering a context manager.
+
+    It returns `RuntimeError("generator didn't yield")` on Python 3.10.
+    '''
 
     @contextmanager
     def ctx() -> Iterator[None]:
