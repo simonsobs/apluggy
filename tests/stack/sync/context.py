86a36a9f5e5555b08a2d664741f9345db953a190
Tai Sakuma <tai.sakuma@gmail.com>
2025-02-13 13:58:59 -0500

    Split `context.py` into two modules

--- tests/stack/sync/context.py
+++ tests/stack/sync/context.py
@@ -15,79 +15,11 @@ else:
     from typing_extensions import TypeAlias
 
 
-from .ctx_id import ContextIdGenerator, CtxId
+from .ctx_id import CtxId
 from .exc import MockException, wrap_exc
 from .except_ import EXCEPT_ACTIONS, ExceptActionName
 from .exit_ import ExitHandler
-
-
-class Created:
-    def __init__(self) -> None:
-        self._ctx_ids: list[CtxId] = []
-        self._ctxs: list[GenCtxMngr] = []
-        self._new_ctx_id = ContextIdGenerator()
-
-    @property
-    def ctx_ids(self) -> list[CtxId]:
-        return list(self._ctx_ids)
-
-    def add(self, ctx: GenCtxMngr) -> CtxId:
-        ctx_id = self._new_ctx_id()
-        self._ctx_ids.append(ctx_id)
-        self._ctxs.append(ctx)
-        return ctx_id
-
-    def assert_on_created(self, ctxs: Iterable[GenCtxMngr]) -> None:
-        assert list(ctxs) == self._ctxs
-
-
-class Entered:
-    def __init__(
-        self,
-        ctx_ids_expected: Iterable[CtxId] = (),
-        yields_expected: Iterable[str] = (),
-    ) -> None:
-        self._ctx_ids_expected = list(ctx_ids_expected)
-        self._yields_expected = list(yields_expected)
-        self._ctx_ids: list[CtxId] = []
-
-    @property
-    def ctx_ids(self) -> list[CtxId]:
-        return list(self._ctx_ids)
-
-    def add(self, ctx_id: CtxId) -> None:
-        self._ctx_ids.append(ctx_id)
-
-    def assert_on_entered(self, yields: Iterable[str]) -> None:
-        assert self._ctx_ids == self._ctx_ids_expected
-        assert list(yields) == self._yields_expected
-
-
-class Sent:
-    def __init__(self, sent_expected: str, yields_expected: Iterable[str]) -> None:
-        self._sent_expected = sent_expected
-        self._yields_expected = list(yields_expected)
-        self._sent_actual: list[str] = []
-
-    def add(self, sent: str) -> None:
-        self._sent_actual.append(sent)
-
-    def assert_on_sent(self, yields: Iterable[str]) -> None:
-        assert len(self._sent_actual) == len(self._yields_expected)
-        assert set(self._sent_actual) == {self._sent_expected}
-        assert list(yields) == self._yields_expected
-
-
-class SentNull(Sent):
-    def __init__(self) -> None:
-        pass
-
-    def add(self, sent: str) -> None:
-        pass
-
-    def assert_on_sent(self, yields: Iterable[str]) -> None:
-        assert False  # pragma: no cover
-
+from .states import Created, Entered, Sent, SentNull
 
 CtxActionName = Literal['yield', 'raise', 'exit']
 CTX_ACTIONS: Sequence[CtxActionName] = ('yield', 'raise', 'exit')
---
+++ tests/stack/sync/states.py
@@ -0,0 +1,73 @@
+from collections.abc import Iterable
+
+from apluggy.stack import GenCtxMngr
+
+from .ctx_id import ContextIdGenerator, CtxId
+
+
+class Created:
+    def __init__(self) -> None:
+        self._ctx_ids: list[CtxId] = []
+        self._ctxs: list[GenCtxMngr] = []
+        self._new_ctx_id = ContextIdGenerator()
+
+    @property
+    def ctx_ids(self) -> list[CtxId]:
+        return list(self._ctx_ids)
+
+    def add(self, ctx: GenCtxMngr) -> CtxId:
+        ctx_id = self._new_ctx_id()
+        self._ctx_ids.append(ctx_id)
+        self._ctxs.append(ctx)
+        return ctx_id
+
+    def assert_on_created(self, ctxs: Iterable[GenCtxMngr]) -> None:
+        assert list(ctxs) == self._ctxs
+
+
+class Entered:
+    def __init__(
+        self,
+        ctx_ids_expected: Iterable[CtxId] = (),
+        yields_expected: Iterable[str] = (),
+    ) -> None:
+        self._ctx_ids_expected = list(ctx_ids_expected)
+        self._yields_expected = list(yields_expected)
+        self._ctx_ids: list[CtxId] = []
+
+    @property
+    def ctx_ids(self) -> list[CtxId]:
+        return list(self._ctx_ids)
+
+    def add(self, ctx_id: CtxId) -> None:
+        self._ctx_ids.append(ctx_id)
+
+    def assert_on_entered(self, yields: Iterable[str]) -> None:
+        assert self._ctx_ids == self._ctx_ids_expected
+        assert list(yields) == self._yields_expected
+
+
+class Sent:
+    def __init__(self, sent_expected: str, yields_expected: Iterable[str]) -> None:
+        self._sent_expected = sent_expected
+        self._yields_expected = list(yields_expected)
+        self._sent_actual: list[str] = []
+
+    def add(self, sent: str) -> None:
+        self._sent_actual.append(sent)
+
+    def assert_on_sent(self, yields: Iterable[str]) -> None:
+        assert len(self._sent_actual) == len(self._yields_expected)
+        assert set(self._sent_actual) == {self._sent_expected}
+        assert list(yields) == self._yields_expected
+
+
+class SentNull(Sent):
+    def __init__(self) -> None:
+        pass
+
+    def add(self, sent: str) -> None:
+        pass
+
+    def assert_on_sent(self, yields: Iterable[str]) -> None:
+        assert False  # pragma: no cover
