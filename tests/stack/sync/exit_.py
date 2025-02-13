b24ff1c7a918f03f8af8224e218266c2b1a6c0d6
Tai Sakuma <tai.sakuma@gmail.com>
2025-02-13 08:22:40 -0500

    Extract `exit_.py` from `context.py`

---
+++ tests/stack/sync/exit_.py
@@ -0,0 +1,160 @@
+from collections.abc import Iterable, Sequence
+from typing import Union
+
+from hypothesis import strategies as st
+
+from .ctx_id import CtxId
+from .exc import ExceptionExpectation, GeneratorDidNotYield, wrap_exc
+from .except_ import (
+    ExceptActionName,
+    ExceptionHandler,
+    ExceptionHandlerNull,
+    st_exception_handler,
+)
+
+
+class ExitHandler:
+    def __init__(
+        self,
+        data: st.DataObject,
+        enabled_except_actions_on_enter: Sequence[ExceptActionName],
+        enabled_except_actions_on_sent: Sequence[ExceptActionName],
+        enabled_except_actions_on_raised: Sequence[ExceptActionName],
+    ) -> None:
+        self._draw = data.draw
+        self._enabled_except_actions_on_enter = enabled_except_actions_on_enter
+        self._enabled_except_actions_on_sent = enabled_except_actions_on_sent
+        self._enabled_except_actions_on_raised = enabled_except_actions_on_raised
+
+        self._to_be_exited = False
+        self._ctx_ids: list[CtxId] = []
+
+    def expect_exit_on_enter(self, entered_ctx_ids: Sequence[CtxId]) -> None:
+        # The last entered context exits without yielding.
+        self.expect_raise_on_enter(entered_ctx_ids, wrap_exc(GeneratorDidNotYield))
+
+    def expect_raise_on_enter(
+        self, entered_ctx_ids: Sequence[CtxId], exp_exc: ExceptionExpectation
+    ) -> None:
+        # The last entered context raises an exception.
+        ctx_ids_reversed = list(reversed(entered_ctx_ids))
+        suspended_ctx_ids = ctx_ids_reversed[1:]
+
+        exc_handler = self._draw(
+            st_exception_handler(
+                exp=exp_exc,
+                ids=suspended_ctx_ids,
+                enabled_actions=self._enabled_except_actions_on_enter,
+            )
+        )
+
+        exp_on_handle = wrap_exc(GeneratorDidNotYield)
+        exc_expected = exc_handler.expect_outermost_exc(exp_on_handle=exp_on_handle)
+
+        self.expect_to_exit_on_error(
+            ctx_ids=ctx_ids_reversed,
+            exc_expected=exc_expected,
+            exc_handler=exc_handler,
+        )
+
+    def expect_raise_in_with_block(
+        self, entered_ctx_ids: Sequence[CtxId], exp_exc: ExceptionExpectation
+    ):
+        exc_handler = self._draw(
+            st_exception_handler(
+                exp=exp_exc,
+                ids=reversed(entered_ctx_ids),
+                enabled_actions=self._enabled_except_actions_on_raised,
+            )
+        )
+
+        exc_expected = exc_handler.expect_outermost_exc()
+
+        self.expect_to_exit_on_error(
+            ctx_ids=reversed(entered_ctx_ids),
+            exc_expected=exc_expected,
+            exc_handler=exc_handler,
+        )
+
+    def expect_send_without_ctx(self) -> None:
+        exc_handler = ExceptionHandlerNull()
+        exc_expected = wrap_exc(StopIteration())
+        self.expect_to_exit_on_error(
+            ctx_ids=[], exc_expected=exc_expected, exc_handler=exc_handler
+        )
+
+    def expect_exit_on_sent(
+        self,
+        exiting_ctx_id: CtxId,
+        entered_ctx_ids: Sequence[CtxId],
+    ) -> None:
+        suspended_ctx_ids = [id for id in entered_ctx_ids if id != exiting_ctx_id]
+        exc_handler = ExceptionHandlerNull()
+        exc_expected = wrap_exc(StopIteration())
+        self.expect_to_exit_on_error(
+            ctx_ids=[exiting_ctx_id, *reversed(suspended_ctx_ids)],
+            exc_expected=exc_expected,
+            exc_handler=exc_handler,
+        )
+
+    def expect_raise_on_sent(
+        self,
+        raising_ctx_id: CtxId,
+        entered_ctx_ids: Sequence[CtxId],
+        exp_exc: ExceptionExpectation,
+    ) -> None:
+        suspended_ctx_ids = [id for id in entered_ctx_ids if id != raising_ctx_id]
+        if not suspended_ctx_ids:
+            exc_handler: ExceptionHandler = ExceptionHandlerNull()
+            exc_expected = exp_exc
+        else:
+            exc_handler = self._draw(
+                st_exception_handler(
+                    exp=exp_exc,
+                    ids=reversed(suspended_ctx_ids),
+                    enabled_actions=self._enabled_except_actions_on_sent,
+                )
+            )
+            exp_on_handle = wrap_exc(StopIteration())
+            exc_expected = exc_handler.expect_outermost_exc(exp_on_handle=exp_on_handle)
+
+        self.expect_to_exit_on_error(
+            ctx_ids=[raising_ctx_id, *reversed(suspended_ctx_ids)],
+            exc_expected=exc_expected,
+            exc_handler=exc_handler,
+        )
+
+    def expect_to_exit_on_error(
+        self,
+        ctx_ids: Iterable[CtxId],
+        exc_expected: ExceptionExpectation,
+        exc_handler: ExceptionHandler,
+    ) -> None:
+        self._to_be_exited = True
+        self._ctx_ids_expected = list(ctx_ids)
+        self._exc_expected = exc_expected
+        self._exc_handler = exc_handler
+
+    def expect_to_exit(self, ctx_ids: Iterable[CtxId]) -> None:
+        self._to_be_exited = True
+        self._ctx_ids_expected = list(ctx_ids)
+        self._exc_expected = wrap_exc(None)
+        self._exc_handler = ExceptionHandlerNull()
+
+    def on_error(self, id: CtxId, exc: Exception) -> None:
+        self._exc_handler.handle(id, exc)
+
+    def on_exiting(self, ctx_id: CtxId) -> None:
+        self._ctx_ids.append(ctx_id)
+
+    def assert_on_entered(self) -> None:
+        assert not self._to_be_exited
+
+    def assert_on_sent(self) -> None:
+        assert not self._to_be_exited
+
+    def assert_on_exited(self, exc: Union[BaseException, None]) -> None:
+        assert self._to_be_exited
+        assert self._ctx_ids == self._ctx_ids_expected
+        assert self._exc_expected == exc
+        self._exc_handler.assert_on_exited(exc)
