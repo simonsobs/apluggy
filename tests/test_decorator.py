755ee3db2918135b23cf75f5b7362d94e312562c
Tai Sakuma <tai.sakuma@gmail.com>
2025-02-17 11:55:35 -0500

    Deprecate `apluggy.asynccontextmanager`

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -1,11 +1,7 @@
+import pytest
 from pluggy._hooks import varnames
 
 from apluggy import asynccontextmanager, contextmanager
-
-
-@asynccontextmanager
-async def acontext(arg1, arg2):
-    yield arg1 + arg2
 
 
 @contextmanager
@@ -14,6 +10,11 @@ def context(arg1, arg2):
 
 
 async def test_acontext():
+    with pytest.deprecated_call():
+        @asynccontextmanager
+        async def acontext(arg1, arg2):
+            yield arg1 + arg2
+
     expected = (('arg1', 'arg2'), ())
     assert varnames(acontext) == expected
     async with acontext(1, 2) as result:
