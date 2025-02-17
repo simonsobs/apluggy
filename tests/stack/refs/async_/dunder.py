03de8f431c5f578da6e42ea907d8361c5bc520fc
Tai Sakuma <tai.sakuma@gmail.com>
2025-02-17 10:51:37 -0500

    Remove the option `fix_reraise` from `async_stack_gen_ctxs()` as it is no-op

--- tests/stack/refs/async_/dunder.py
+++ tests/stack/refs/async_/dunder.py
@@ -4,32 +4,24 @@ import sys
 from collections.abc import AsyncGenerator, Iterable
 from typing import Any, TypeVar
 
-from apluggy.stack import patch_aexit
-
 from .types import AGenCtxMngr
 
 T = TypeVar('T')
 
 
 def async_stack_dunder_enter(
-    ctxs: Iterable[AGenCtxMngr[T]],
-    fix_reraise: bool = True,
-    sequential: bool = True,
+    ctxs: Iterable[AGenCtxMngr[T]], sequential: bool = True
 ) -> AGenCtxMngr[list[T]]:
     '''A reference implementation of `async_stack_gen_ctxs` for tests.'''
     ctxs = list(ctxs)
     if not ctxs:
         return async_stack_dunder_enter_null(ctxs)
     if len(ctxs) == 1:
-        return async_stack_dunder_enter_single(ctxs, fix_reraise=fix_reraise)
+        return async_stack_dunder_enter_single(ctxs)
     if len(ctxs) == 2:
-        return async_stack_dunder_enter_double(
-            ctxs, fix_reraise=fix_reraise, sequential=sequential
-        )
+        return async_stack_dunder_enter_double(ctxs, sequential=sequential)
     if len(ctxs) == 3:
-        return async_stack_dunder_enter_triple(
-            ctxs, fix_reraise=fix_reraise, sequential=sequential
-        )
+        return async_stack_dunder_enter_triple(ctxs, sequential=sequential)
     raise NotImplementedError()
 
 
@@ -45,32 +37,28 @@ async def async_stack_dunder_enter_null(
 @contextlib.asynccontextmanager
 async def async_stack_dunder_enter_single(
     ctxs: Iterable[AGenCtxMngr[T]],
-    fix_reraise: bool,
 ) -> AsyncGenerator[list[T], Any]:
     ctxs = list(ctxs)
     assert len(ctxs) == 1
     ctx = ctxs[0]
     y = await ctx.__aenter__()
-    with patch_aexit(ctx) if fix_reraise else contextlib.nullcontext():
+    try:
+        sent = yield [y]
         try:
-            sent = yield [y]
-            try:
-                while True:
-                    sent = yield [await ctx.gen.asend(sent)]
-            except StopAsyncIteration:
-                pass
-        except BaseException:
-            if not await ctx.__aexit__(*sys.exc_info()):
-                raise
-        else:
-            await ctx.__aexit__(None, None, None)
+            while True:
+                sent = yield [await ctx.gen.asend(sent)]
+        except StopAsyncIteration:
+            pass
+    except BaseException:
+        if not await ctx.__aexit__(*sys.exc_info()):
+            raise
+    else:
+        await ctx.__aexit__(None, None, None)
 
 
 @contextlib.asynccontextmanager
 async def async_stack_dunder_enter_double(
-    ctxs: Iterable[AGenCtxMngr[T]],
-    fix_reraise: bool,
-    sequential: bool = True,
+    ctxs: Iterable[AGenCtxMngr[T]], sequential: bool = True
 ) -> AsyncGenerator[list[T], Any]:
     ctxs = list(ctxs)
     assert len(ctxs) == 2
@@ -82,57 +70,51 @@ async def async_stack_dunder_enter_double(
         entered.add(ctx)
         return y
 
-    with contextlib.ExitStack() as stack:
-        if fix_reraise:
-            stack.enter_context(patch_aexit(ctx0))
-            stack.enter_context(patch_aexit(ctx1))
+    try:
         try:
-            try:
-                if sequential:
-                    ys = [await _enter(ctx0), await _enter(ctx1)]
-                else:
-                    ys = list(await asyncio.gather(_enter(ctx0), _enter(ctx1)))
-                sent = yield ys
-                try:
-                    while True:
-                        if sequential:
-                            ys = [
-                                await ctx1.gen.asend(sent),
-                                await ctx0.gen.asend(sent),
-                            ]
-                        else:
-                            ys = list(
-                                await asyncio.gather(
-                                    ctx1.gen.asend(sent),
-                                    ctx0.gen.asend(sent),
-                                )
-                            )
-                        sent = yield ys
-                except StopAsyncIteration:
-                    pass
-            except BaseException:
-                if ctx1 not in entered:
-                    raise
-                if not await ctx1.__aexit__(*sys.exc_info()):
-                    raise
+            if sequential:
+                ys = [await _enter(ctx0), await _enter(ctx1)]
             else:
-                if ctx1 in entered:
-                    await ctx1.__aexit__(None, None, None)
+                ys = list(await asyncio.gather(_enter(ctx0), _enter(ctx1)))
+            sent = yield ys
+            try:
+                while True:
+                    if sequential:
+                        ys = [
+                            await ctx1.gen.asend(sent),
+                            await ctx0.gen.asend(sent),
+                        ]
+                    else:
+                        ys = list(
+                            await asyncio.gather(
+                                ctx1.gen.asend(sent),
+                                ctx0.gen.asend(sent),
+                            )
+                        )
+                    sent = yield ys
+            except StopAsyncIteration:
+                pass
         except BaseException:
-            if ctx0 not in entered:
+            if ctx1 not in entered:
                 raise
-            if not await ctx0.__aexit__(*sys.exc_info()):
+            if not await ctx1.__aexit__(*sys.exc_info()):
                 raise
         else:
-            if ctx0 in entered:
-                await ctx0.__aexit__(None, None, None)
+            if ctx1 in entered:
+                await ctx1.__aexit__(None, None, None)
+    except BaseException:
+        if ctx0 not in entered:
+            raise
+        if not await ctx0.__aexit__(*sys.exc_info()):
+            raise
+    else:
+        if ctx0 in entered:
+            await ctx0.__aexit__(None, None, None)
 
 
 @contextlib.asynccontextmanager
 async def async_stack_dunder_enter_triple(  # noqa: C901
-    ctxs: Iterable[AGenCtxMngr[T]],
-    fix_reraise: bool,
-    sequential: bool = True,
+    ctxs: Iterable[AGenCtxMngr[T]], sequential: bool = True
 ) -> AsyncGenerator[list[T], Any]:
     ctxs = list(ctxs)
     assert len(ctxs) == 3
@@ -144,67 +126,60 @@ async def async_stack_dunder_enter_triple(  # noqa: C901
         entered.add(ctx)
         return y
 
-    with contextlib.ExitStack() as stack:
-        if fix_reraise:
-            stack.enter_context(patch_aexit(ctx0))
-            stack.enter_context(patch_aexit(ctx1))
-            stack.enter_context(patch_aexit(ctx2))
+    try:
         try:
             try:
-                try:
-                    if sequential:
-                        ys = [
-                            await _enter(ctx0),
-                            await _enter(ctx1),
-                            await _enter(ctx2),
-                        ]
-                    else:
-                        ys = list(
-                            await asyncio.gather(
-                                _enter(ctx0), _enter(ctx1), _enter(ctx2)
-                            )
-                        )
-                    sent = yield ys
-                    try:
-                        while True:
-                            if sequential:
-                                ys = [
-                                    await ctx2.gen.asend(sent),
-                                    await ctx1.gen.asend(sent),
-                                    await ctx0.gen.asend(sent),
-                                ]
-                            else:
-                                ys = list(
-                                    await asyncio.gather(
-                                        ctx2.gen.asend(sent),
-                                        ctx1.gen.asend(sent),
-                                        ctx0.gen.asend(sent),
-                                    )
-                                )
-                            sent = yield ys
-                    except StopAsyncIteration:
-                        pass
-                except BaseException:
-                    if ctx2 not in entered:
-                        raise
-                    if not await ctx2.__aexit__(*sys.exc_info()):
-                        raise
+                if sequential:
+                    ys = [
+                        await _enter(ctx0),
+                        await _enter(ctx1),
+                        await _enter(ctx2),
+                    ]
                 else:
-                    if ctx2 in entered:
-                        await ctx2.__aexit__(None, None, None)
+                    ys = list(
+                        await asyncio.gather(_enter(ctx0), _enter(ctx1), _enter(ctx2))
+                    )
+                sent = yield ys
+                try:
+                    while True:
+                        if sequential:
+                            ys = [
+                                await ctx2.gen.asend(sent),
+                                await ctx1.gen.asend(sent),
+                                await ctx0.gen.asend(sent),
+                            ]
+                        else:
+                            ys = list(
+                                await asyncio.gather(
+                                    ctx2.gen.asend(sent),
+                                    ctx1.gen.asend(sent),
+                                    ctx0.gen.asend(sent),
+                                )
+                            )
+                        sent = yield ys
+                except StopAsyncIteration:
+                    pass
             except BaseException:
-                if ctx1 not in entered:
+                if ctx2 not in entered:
                     raise
-                if not await ctx1.__aexit__(*sys.exc_info()):
+                if not await ctx2.__aexit__(*sys.exc_info()):
                     raise
             else:
-                if ctx1 in entered:
-                    await ctx1.__aexit__(None, None, None)
+                if ctx2 in entered:
+                    await ctx2.__aexit__(None, None, None)
         except BaseException:
-            if ctx0 not in entered:
+            if ctx1 not in entered:
                 raise
-            if not await ctx0.__aexit__(*sys.exc_info()):
+            if not await ctx1.__aexit__(*sys.exc_info()):
                 raise
         else:
-            if ctx0 in entered:
-                await ctx0.__aexit__(None, None, None)
+            if ctx1 in entered:
+                await ctx1.__aexit__(None, None, None)
+    except BaseException:
+        if ctx0 not in entered:
+            raise
+        if not await ctx0.__aexit__(*sys.exc_info()):
+            raise
+    else:
+        if ctx0 in entered:
+            await ctx0.__aexit__(None, None, None)
